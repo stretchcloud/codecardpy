8fcc008fb9d00f5d72f55a019aefcbf21601ac3e
jit2600@gmail.com <jit2600@gmail.com>
2019-05-02 10:10:05 +0100

    final changes to stack.yml & added env variable

--- codecardpy/handler.py
+++ codecardpy/handler.py
@@ -1,6 +1,5 @@
 import json
-def codecardjson():
-    x = {"template": "template4", "title": "Prasenjit Sarkar", "subtitle": "Cloud Native Evangelist", "bodytext": "Ask me anything on Cloud Native Application World! I run blog.kube-mesh.io", "icon": "microservices", "backgroundColor": "white"}
-    y = json.dumps(x)
-    print(y)
-codecardjson()
+
+def handle(req):
+    data = {'template': 'template4', 'title': 'Prasenjit Sarkar', 'subtitle': 'Cloud Native Evangelist', 'bodytext': 'Ask me anything on Cloud Native Application World! I run blog.kube-mesh.io', 'icon': 'microservices', 'backgroundColor': 'white'}
+    return json.dumps(data)
