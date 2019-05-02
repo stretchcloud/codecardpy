5db95fa506aa9a298e4920baa85eb8f675d0224a
jit2600@gmail.com <jit2600@gmail.com>
2019-05-02 09:12:35 +0100

    added json module and changed the handler code

--- codecardpy/handler.py
+++ codecardpy/handler.py
@@ -1,7 +1,6 @@
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-
-    return req
+import json
+def codecardjson():
+    x = {"template": "template4", "title": "Prasenjit Sarkar", "subtitle": "Cloud Native Evangelist", "bodytext": "Ask me anything on Cloud Native Application World! I run blog.kube-mesh.io", "icon": "microservices", "backgroundColor": "white"}
+    y = json.dumps(x)
+    print(y)
+codecardjson()
